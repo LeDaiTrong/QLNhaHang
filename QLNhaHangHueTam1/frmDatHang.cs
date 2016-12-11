db5cf2ba2c752218d6bb94bc5a69eaf72df605d7
LeDaiTrong <1454050156trong@ou.edu.vn>
2016-12-12 02:05:52 +0700

    lam xong DatHang

---
+++ QLNhaHangHueTam1/frmDatHang.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace NhanVien
+{
+    public partial class frmDatHang : Form
+    {
+        string cnStr;
+        SqlConnection cn;
+        //DataSet ds;
+        //DataTable dt = new DataTable();
+        public frmDatHang()
+        {
+            InitializeComponent();
+        }
+
+        private void frmDatHang_Load(object sender, EventArgs e)
+        {
+            cnStr = "Server = .; Database = QuanLyATC_HueTam; Integrated security = true;";
+            cn = new SqlConnection();
+            cn.ConnectionString = cnStr;
+            List<DatHang> lst = DonDatHang();
+            dgvDatHang.DataSource = lst;
+        }
+
+        public List<DatHang> DonDatHang()
+        {
+            cn.Open();
+            List<DatHang> list = new List<DatHang>();
+            SqlCommand cmd = new SqlCommand();
+            cmd.CommandText = "uspDatHang";
+            cmd.Connection = cn;
+            cmd.CommandType = CommandType.StoredProcedure;
+            SqlDataReader dr = cmd.ExecuteReader();
+            string MaNVL, TenNVL, LoaiNVL, Quycach, dvt;
+            //float slcon;
+            while (dr.Read())
+            {
+                MaNVL = dr.GetString(0);
+                TenNVL = dr.GetString(1);
+                LoaiNVL = dr.GetString(2);
+                Quycach = dr.GetString(3);
+                dvt = dr.GetString(4);
+                //slcon = dr.GetFloat(5);
+                DatHang DH1 = new DatHang(MaNVL, TenNVL, LoaiNVL, Quycach, dvt);
+                list.Add(DH1);
+            }
+            dr.Close();
+            return list;
+        }
+    }
+}
---
+++ QLNhaHangHueTam2/frmDatHang.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace NhanVien
+{
+    partial class frmDatHang
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.dgvDatHang = new System.Windows.Forms.DataGridView();
+            this.dataSet1BindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatHang)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1BindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvDatHang
+            // 
+            this.dgvDatHang.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvDatHang.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDatHang.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1});
+            this.dgvDatHang.Location = new System.Drawing.Point(0, 147);
+            this.dgvDatHang.Name = "dgvDatHang";
+            this.dgvDatHang.Size = new System.Drawing.Size(776, 386);
+            this.dgvDatHang.TabIndex = 0;
+            // 
+            // Column1
+            // 
+            this.Column1.AutoSizeMode = System.Windows.Forms.DataGridViewAutoSizeColumnMode.Fill;
+            dataGridViewCellStyle1.NullValue = null;
+            this.Column1.DefaultCellStyle = dataGridViewCellStyle1;
+            this.Column1.HeaderText = "STT";
+            this.Column1.Name = "Column1";
+            // 
+            // frmDatHang
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(776, 533);
+            this.Controls.Add(this.dgvDatHang);
+            this.Name = "frmDatHang";
+            this.Text = "Đặt hàng";
+            this.Load += new System.EventHandler(this.frmDatHang_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDatHang)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dataSet1BindingSource)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvDatHang;
+        private System.Windows.Forms.BindingSource dataSet1BindingSource;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+    }
+}
