1ff2ea928e452cd5daae0419700adfe59fef5ce1
LeDaiTrong <1454050156trong@ou.edu.vn>
2016-12-16 00:54:52 +0700

    TongTien chua chay dc, con lai chay dc

---
+++ QLNhaHangHueTam/MainFrm.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace NhanVien
+{
+    public partial class MainFrm : Form
+    {
+        public MainFrm()
+        {
+            frmDelay f = new frmDelay();
+            f.ShowDialog();
+            InitializeComponent();
+        }
+    }
+}
