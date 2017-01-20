89a50fbee189b5dbfc857935e323c106859fc9fb
LeDaiTrong <1454050156trong@ou.edu.vn>
2017-01-20 23:11:59 +0700

    nop ma nguon, script CSDL, huong dan su dung

---
+++ QLNH_NopMaNguon_CSDL_HDSD/QLNhaHangHueTam/frmHoaDon.Designer.cs
@@ -0,0 +1,218 @@
+﻿namespace NhanVien
+{
+    partial class frmHoaDon
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmHoaDon));
+            this.dgvHoaDon = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbMaHD = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.dtpNgayLapHD = new System.Windows.Forms.DateTimePicker();
+            this.label5 = new System.Windows.Forms.Label();
+            this.lbTongCong = new System.Windows.Forms.Label();
+            this.txtMaKH = new System.Windows.Forms.TextBox();
+            this.txtMaNV = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtBanSo = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHoaDon)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dgvHoaDon
+            // 
+            this.dgvHoaDon.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+                        | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.dgvHoaDon.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvHoaDon.BackgroundColor = System.Drawing.Color.LightCyan;
+            this.dgvHoaDon.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvHoaDon.Location = new System.Drawing.Point(0, 85);
+            this.dgvHoaDon.Name = "dgvHoaDon";
+            this.dgvHoaDon.Size = new System.Drawing.Size(763, 369);
+            this.dgvHoaDon.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(8, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 33);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Mã hóa đơn";
+            // 
+            // label2
+            // 
+            this.label2.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.label2.Location = new System.Drawing.Point(269, 16);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(97, 33);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mã khách hàng";
+            // 
+            // label3
+            // 
+            this.label3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label3.Location = new System.Drawing.Point(540, 16);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(97, 33);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Mã nhân viên";
+            // 
+            // cbMaHD
+            // 
+            this.cbMaHD.FormattingEnabled = true;
+            this.cbMaHD.Location = new System.Drawing.Point(102, 11);
+            this.cbMaHD.Name = "cbMaHD";
+            this.cbMaHD.Size = new System.Drawing.Size(121, 21);
+            this.cbMaHD.TabIndex = 2;
+            this.cbMaHD.SelectedIndexChanged += new System.EventHandler(this.cbMaHD_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.Location = new System.Drawing.Point(8, 49);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(97, 33);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Ngày lập HD";
+            // 
+            // dtpNgayLapHD
+            // 
+            this.dtpNgayLapHD.CustomFormat = "hh:mm - dd/mm/yyyy";
+            this.dtpNgayLapHD.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpNgayLapHD.Location = new System.Drawing.Point(102, 44);
+            this.dtpNgayLapHD.Name = "dtpNgayLapHD";
+            this.dtpNgayLapHD.Size = new System.Drawing.Size(131, 20);
+            this.dtpNgayLapHD.TabIndex = 3;
+            // 
+            // label5
+            // 
+            this.label5.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(543, 471);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(82, 21);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Tổng cộng:";
+            // 
+            // lbTongCong
+            // 
+            this.lbTongCong.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.lbTongCong.BackColor = System.Drawing.Color.MediumSeaGreen;
+            this.lbTongCong.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.lbTongCong.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbTongCong.Location = new System.Drawing.Point(631, 469);
+            this.lbTongCong.Name = "lbTongCong";
+            this.lbTongCong.Size = new System.Drawing.Size(124, 44);
+            this.lbTongCong.TabIndex = 5;
+            // 
+            // txtMaKH
+            // 
+            this.txtMaKH.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMaKH.Location = new System.Drawing.Point(372, 11);
+            this.txtMaKH.Name = "txtMaKH";
+            this.txtMaKH.Size = new System.Drawing.Size(121, 20);
+            this.txtMaKH.TabIndex = 6;
+            // 
+            // txtMaNV
+            // 
+            this.txtMaNV.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtMaNV.Location = new System.Drawing.Point(634, 12);
+            this.txtMaNV.Name = "txtMaNV";
+            this.txtMaNV.Size = new System.Drawing.Size(121, 20);
+            this.txtMaNV.TabIndex = 6;
+            // 
+            // label6
+            // 
+            this.label6.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.label6.Location = new System.Drawing.Point(306, 44);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(50, 33);
+            this.label6.TabIndex = 1;
+            this.label6.Text = "Bàn số";
+            // 
+            // txtBanSo
+            // 
+            this.txtBanSo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left)
+                        | System.Windows.Forms.AnchorStyles.Right)));
+            this.txtBanSo.Location = new System.Drawing.Point(372, 39);
+            this.txtBanSo.Name = "txtBanSo";
+            this.txtBanSo.Size = new System.Drawing.Size(121, 20);
+            this.txtBanSo.TabIndex = 6;
+            // 
+            // frmHoaDon
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.PowderBlue;
+            this.ClientSize = new System.Drawing.Size(763, 522);
+            this.Controls.Add(this.txtMaNV);
+            this.Controls.Add(this.txtBanSo);
+            this.Controls.Add(this.txtMaKH);
+            this.Controls.Add(this.lbTongCong);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.dtpNgayLapHD);
+            this.Controls.Add(this.cbMaHD);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dgvHoaDon);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmHoaDon";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chi tiết hóa đơn";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.frmHoaDon_FormClosed);
+            this.Load += new System.EventHandler(this.frmHoaDon_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvHoaDon)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dgvHoaDon;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbMaHD;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.DateTimePicker dtpNgayLapHD;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lbTongCong;
+        private System.Windows.Forms.TextBox txtMaKH;
+        private System.Windows.Forms.TextBox txtMaNV;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtBanSo;
+    }
+}
